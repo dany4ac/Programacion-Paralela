0d6d57b53b95ccaebe1d563e8c7867865e723c59
dany4ac <dany4ac@gmail.com>
2020-01-26 23:33:00 -0500

    update go

--- Labo3/libro_pag85/src/libro_pag85.c
+++ Labo3/libro_pag85/src/libro_pag85.c
@@ -23,10 +23,10 @@ int main(void){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	
 	if(my_rank != 0){
-		sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
+		sprintf(greeting, "Saludos desde el proceso %d of %d!", my_rank, comm_sz);
 		MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 	}else{
-		printf("Greetings from process %d of %d!\n", my_rank, comm_sz);
+		printf("Saludos desde el proceso %d of %d!\n", my_rank, comm_sz);
 		for(int q = 1; q < comm_sz; q++){
 			MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			printf("%s\n", greeting);
